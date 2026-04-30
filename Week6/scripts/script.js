2da112622f06c5ca1d6ef368372c919961fb0b49
MillerOvermoyer <86533463+MillerOvermoyer@users.noreply.github.com>
2026-04-30 17:27:02 -0400

    wekk6

--- Week6/scripts/script.js
+++ Week6/scripts/script.js
@@ -1,3 +1,72 @@
-function myFunction() {
-    
+
+var taco = 10
+
+var favoriteFood = "Korean BBQ"
+
+var exampleBool = true
+
+console.log("Hello")
+
+//conditional operators
+//
+//
+
+var x = 19
+var y = 20
+
+if (x > y) {
+    console.log("X is greater than Y")
 }
+else {
+    console.log("X is not greater than Y")
+}
+
+var numOne = 20
+var numTwo = 6
+
+numOne = numOne + numTwo
+console.log(numOne)
+
+numOne += numTwo
+console.log(numOne)
+
+numOne *= 2
+console.log(numOne)
+
+function riceBalls()
+{
+    console.log("Hello World!")
+}
+
+riceBalls()
+riceBalls()
+
+function add(paramOne, paramTwo){
+    console.log(paramOne + paramTwo)
+}
+
+add(3, 5)
+
+var rice = 256
+var miller = 1
+
+add(rice, miller)
+
+function pizzaCookie(a,b,c){
+    return a * b * c
+}
+
+
+var result = pizzaCookie()
+
+alert("This is an alert")
+
+//a prompt will give you a string back
+var name = prompt("input something here")
+console.log(name)
+
+//when you add strings together they concatonate
+var p = Number(prompt("Enter a number"))
+var q = Number(prompt("enter another numer"))
+
+console.log(p + q)
