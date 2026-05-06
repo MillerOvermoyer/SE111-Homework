2303388e8558ae0f450223317e5ca5288f69d7c8
MillerOvermoyer <86533463+MillerOvermoyer@users.noreply.github.com>
2026-05-06 13:02:41 -0400

    finished basic calculator with javascript

--- Week6/scripts/script.js
+++ Week6/scripts/script.js
@@ -1,79 +1,6 @@
-
-var taco = 10
-
-var favoriteFood = "Korean BBQ"
-
-var exampleBool = true
-
-console.log("Hello")
-
-//conditional operators
-//
-//
-
-var x = 19
-var y = 20
-
-if (x > y) {
-    console.log("X is greater than Y")
-}
-else {
-    console.log("X is not greater than Y")
-}
-
-var numOne = 20
-var numTwo = 6
-
-numOne = numOne + numTwo
-console.log(numOne)
-
-numOne += numTwo
-console.log(numOne)
-
-numOne *= 2
-console.log(numOne)
-
-function riceBalls()
-{
-    console.log("Hello World!")
-}
-
-riceBalls()
-riceBalls()
-
-function add(paramOne, paramTwo){
-    console.log(paramOne + paramTwo)
-}
-
-add(3, 5)
-
-var rice = 256
-var miller = 1
-
-add(rice, miller)
-
-function pizzaCookie(a,b,c){
-    return a * b * c
-}
-
-
-var result = pizzaCookie()
-
-alert("This is an alert")
-
-//a prompt will give you a string back
-var name = prompt("input something here")
-console.log(name)
-
-//when you add strings together they concatonate
-var p = Number(prompt("Enter a number"))
-var q = Number(prompt("enter another numer"))
-
-console.log(p + q)
-
 var func = prompt("Would you like to to add, multiply, divide, or subtract? (+, -, *, /")
-var input1 = prompt("What is the first number? (The number you are subtracting/dividing from)")
-var input2 = prompt("What is the second number? (The number you are subtracting/dividing by)")
+var input1 = Number(prompt("What is the first number? (The number you are subtracting/dividing from)"))
+var input2 = Number(prompt("What is the second number? (The number you are subtracting/dividing by)"))
 
 function addNumbers(num1, num2){
     return num1 + num2;
@@ -88,6 +15,22 @@ function divideNumbers(num1, num2){
     return num1 / num2;
 }
 
+
+var result;
 if(func == "+"){
-    var 
+    result = addNumbers(input1, input2);
+    alert( input1 + " + " + input2 + " = " + result);
 }
+else if(func == "-"){
+    result = subtractNumbers(input1, input2);
+    alert( input1 + " - " + input2 + " = " + result);
+}
+else if(func == "*"){
+    result = multiplyNumbers(input1, input2);
+    alert( input1 + " * " + input2 + " = " + result);
+}
+else if(func == "/"){
+    result = divideNumbers(input1, input2);
+    alert( input1 + " / " + input2 + " = " + result);
+}
+
