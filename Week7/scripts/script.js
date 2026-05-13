eb345244c6274c39559aa0f6c071ee42e20801d4
MillerOvermoyer <86533463+MillerOvermoyer@users.noreply.github.com>
2026-05-13 15:54:38 -0400

    week 7 scripts

--- Week7/scripts/script.js
+++ Week7/scripts/script.js
@@ -4,14 +4,40 @@ var decrementButton = document.querySelector("#decrement")
 
 var currentNumber = 0
 
+//This function will add 1 to the current number and update the text on the page
 function addToNumber(){
     currentNumber++
-    console.log(currentNumber)
+    numberText.innerHTML = currentNumber
 }
 
+//This function will subtract 1 from the current number and update the text on the page
 function subtractFromNumber(){
     currentNumber--
-    console.log(currentNumber)
+    numberText.innerHTML = currentNumber
 }
 
 
+//Set up event listeners for the buttons
+incrementButton.addEventListener("click", addToNumber)
+decrementButton.addEventListener("click", subtractFromNumber)
+
+var givePunchlineButton = document.querySelector("#givePunchline")
+var punchlineText = document.querySelector("#punchline")
+
+function givePunchline(){
+    punchlineText.innerHTML += "<h4>The ice might crack up!</h4>"
+}
+
+givePunchlineButton.addEventListener("click", givePunchline)
+
+var saveTextButton = document.querySelector("#saveTextButton")
+var textInput = document.querySelector("#textInput")
+var savedTextContainer = document.querySelector("#savedTextContainer")
+
+function saveText(){
+    var text = textInput.value
+    savedTextContainer.innerHTML += "<p>" + text + "</p>"
+    textInput.value = ""
+}
+
+saveTextButton.addEventListener("click", saveText)
